453c37d8909d72d065b23838a1bf95a91cacb2ea
Allan Julie Fontes de Oliveira <allanzinhofontes@gmail.com>
2022-05-07 22:58:34 -0300

    fix

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,6 +6,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(
+  express.urlencoded({
+    limit: "10mb",
+    extended: true,
+    parameterLimit: 100000,
+  })
+);
 app.use(routes);
 
 app.listen(process.env.PORT || 3333, () => {
